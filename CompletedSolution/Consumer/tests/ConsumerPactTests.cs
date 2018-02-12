16f3de99bf0c2258f8f5f41513590fdfd76cb45f
Thomas Shipley <thomas.shipley@googlemail.com>
2018-02-12 11:33:56 +0000

    Move files around to a src and tests folder for provider and consumer. Also add mock server config for consumer

---
+++ CompletedSolution/Consumer/tests/ConsumerPactTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class ConsumerPactTests
+    {
+        [Fact]
+        public void It_Handles_Invalid_Date_Param()
+        {
+            
+        }
+    }
+}
