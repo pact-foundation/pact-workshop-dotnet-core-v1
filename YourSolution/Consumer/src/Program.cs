46e124555fb2ca7acee36947b8d19bca4da6f94a
Thomas Shipley <thomas.shipley@googlemail.com>
2018-02-14 14:37:21 +0000

    Created the YourSoultion folder for workshop attendees to have a go at using pact in. Contains everything from CompletedSoultion except the test projects

---
+++ YourSolution/Consumer/src/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateTimeToValidate = "05/01/2018";
+            string baseUri = "http://localhost:9000";
+
+            if(args.Length <= 1)
+            {
+                Console.WriteLine("-------------------");
+                WriteoutArgsUsed(dateTimeToValidate, baseUri);
+                WriteoutUsageInstructions();
+                Console.WriteLine("-------------------");
+            }
+            else
+            {
+                dateTimeToValidate = args[0];
+                baseUri = args[1];
+                Console.WriteLine("-------------------");
+                WriteoutArgsUsed(dateTimeToValidate, baseUri);
+                Console.WriteLine("-------------------");
+            }
+
+            Console.WriteLine("Validating date...");
+            var result = ConsumerApiClient.ValidateDateTimeUsingProviderApi(dateTimeToValidate, baseUri).GetAwaiter().GetResult();
+            var resultContentText = result.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            Console.WriteLine(resultContentText);
+            Console.WriteLine("...Date validation complete. Goodbye.");
+        }
+
+        static private void WriteoutArgsUsed(string datetimeArg, string baseUriArg)
+        {
+            Console.WriteLine($"Running consumer with args: dateTimeToValidate = {datetimeArg}, baseUri = {baseUriArg}");
+        }
+
+        static private void WriteoutUsageInstructions()
+        {
+            Console.WriteLine("To use with your own parameters:");
+            Console.WriteLine("Usage: dotnet run [DateTime To Validate] [Provider Api Uri]");
+            Console.WriteLine("Usage Example: dotnet run 01/01/2018 http://localhost:9000");
+        }
+    }
+}
---
+++ YourSolution/Provider/src/Controllers/ProviderController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Provider.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProviderController : Controller
+    {
+        private IConfiguration _Configuration { get; }
+
+        public ProviderController(IConfiguration configuration)
+        {
+            this._Configuration = configuration;
+        }
+
+        // GET api/provider?validDateTime=[DateTime String]
+        [HttpGet]
+        public IActionResult Get(string validDateTime)
+        {
+            if(String.IsNullOrEmpty(validDateTime))
+            {
+                return BadRequest(new { message = "validDateTime is required" });
+            }
+
+            if(this.DataMissing())
+            {
+                return NotFound();
+            }
+
+            DateTime parsedDateTime;
+
+            try
+            {
+                parsedDateTime = DateTime.Parse(validDateTime);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { message = "validDateTime is not a date or time" });
+            }
+
+            return new JsonResult(new {
+                test = "NO",
+                validDateTime = parsedDateTime.ToString("dd-MM-yyyy HH:mm:ss")
+            });
+        }
+
+        private bool DataMissing()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), @"../../data");
+            string pathWithFile = Path.Combine(path, "somedata.txt");
+
+            return !System.IO.File.Exists(pathWithFile);
+        }
+    }
+}
