a0ee8c483df3c0ee37563e6890d47b85a4f5228c
Hugo Doyon <HugoDoyon@users.noreply.github.com>
2021-01-13 11:02:15 -0500

    Provider test throws error
    Fix Provider test error:

--- CompletedSolution/Provider/tests/TestStartup.cs
+++ CompletedSolution/Provider/tests/TestStartup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using tests.Middleware;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace tests
 {
@@ -19,6 +20,11 @@ namespace tests
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
             services.AddMvc();
         }
 
